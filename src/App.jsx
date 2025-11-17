5c4432fa4d82dd776e1e61036c45dc90f042aa1f
juan pablo aguirre <juanpaaguirre320@hotmail.com>
2025-11-16 21:33:27 -0500

    Repo setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+// rutas de mi app
+
+function App () {
+  return (
+    <div>
+      <p className="text-4xl text-red-600">Probando config inicial</p>
+    </div>
+  )
+}
+
+export default App;
